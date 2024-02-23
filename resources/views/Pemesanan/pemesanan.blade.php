b24db53675b16d816dc7d7d34a01d9e232e075fc
YudhisDev <ekayudhis36@gmail.com>
2024-02-23 10:38:58 +0700

    Menambahkan Fitur Update Pemesanan

--- resources/views/Pemesanan/pemesanan.blade.php
+++ resources/views/Pemesanan/pemesanan.blade.php
@@ -1,40 +1,41 @@
 @extends('layout.main')
 @section('content')
     <main>
-        <div class="container-fluid px-2 mt-5">
-            @if (session()->has('success'))
-                <div class="alert alert-primary" role="alert">
-                    {{ session('success') }}
+        <div class="container-fluid px-4">
+            <div class="container-fluid px-2 mt-5">
+                @if (session()->has('success'))
+                    <div class="alert alert-primary" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @elseif(session()->has('edit'))
+                    <div class="alert alert-warning" role="alert">
+                        {{ session('edit') }}
+                    </div>
+                @elseif(session()->has('delete'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('delete') }}
+                    </div>
+                @endif
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createModal">
+                        Tambah Pemesanan
+                    </button>
                 </div>
-            @elseif(session()->has('edit'))
-                <div class="alert alert-warning" role="alert">
-                    {{ session('edit') }}
-                </div>
-            @elseif(session()->has('delete'))
-                <div class="alert alert-danger" role="alert">
-                    {{ session('delete') }}
-                </div>
-            @endif
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createModal">
-                    Tambah Pemesanan
-                </button>
-            </div>
-            <div class="container-fluid px-4">
                 <div class="card mb-4 mt-4">
                     <div class="card-header">
                         <i class="fas fa-table me-1"></i>
-                        Data Pembeli
+                        Data Pemesanan
                     </div>
                     <div class="card-body">
                         <table id="datatablesSimple">
                             <thead>
                                 <tr>
-                                    <th>No Pesanan</th>
+                                    <th colspan="200">No Pesanan</th>
                                     <th>Nama Pembeli</th>
                                     <th>Nama Rumah</th>
                                     <th>Jumlah Pesanan</th>
                                     <th>Status Pembelian</th>
+                                    <th>Navigasi</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -45,6 +46,16 @@
                                         <td>{{ $item->rumah->nama_rumah }}</td>
                                         <td>{{ $item->jumlah }}</td>
                                         <td>{{ $item->status }}</td>
+                                        <td>
+                                            <button type="button" data-bs-toggle="modal"
+                                                data-bs-target='#editModal{{ $item->id_pesanan }}'
+                                                class="btn btn-outline-warning" role="button"><i class="fas fa-pencil"></i>
+                                                Edit</button>
+                                            <button class="btn btn-outline-danger" data-bs-toggle="modal"
+                                                data-bs-target="#deleteModal{{ $item->id_pesanan }}"><i
+                                                    class="fas fa-trash"></i>
+                                                Hapus</button>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -54,4 +65,5 @@
             </div>
     </main>
     @include('Pemesanan.modals.create')
+    @include('Pemesanan.modals.update')
 @endsection
